5bbbc181aa60fa64cfe0e529f599af7697bf2d16
brooksryan <brooksryan19@gmail.com>
2018-07-24 09:27:46 -0700

    finishing up

--- orchestration.js
+++ orchestration.js
@@ -7,63 +7,6 @@
 
 /* EG "AM I ON A TICK PAGE?" -> TICK PAGE FUNCTION 		     */
 
-
-
-
-// ------------ SECTION FOR TICK LIST DOWNLOAD ------------- //
-//get all user ticks returns an object with userURL, exportURL, 
-function getPredictionForCurrentClimb(){
-
-	console.log("made it to the prediction function")
-
-	//get all user ticks returns an object with userURL, exportURL, 
-	return getAllUserTickUrls()
-
-	.then(function(result){
-
-		//then we download all of the CSVs for those users ticks transorm
-		//the ticks to JSON objects and add them to the array of user data 
-		//as "userTicks"	
-		$("#downloadAllUserTicks").text("Almost There");
-		return userDataFromCSVPromise(result)})
-
-	.then(function(userURLS){
-
-		$("#downloadAllUserTicks").text("Tidying Up");
-
-		return makeBigArray(userURLS)})
-
-	.then(function(newBigCSV){
-
-		$("#downloadAllUserTicks").text("Done!");
-
-		downloadThisRouteCSV(newBigCSV);
-
-		console.log("check out all the urls I just got: ", newBigCSV)
-	
-	})
-
-};
-
-//adds a button that enables you to download a CSV of all of the ticks
-//from all of the users who have ticked a climb from on the  
-//tick page for a climb
-$("div.pt-main-content > div.col-xs-12").append('<button id="downloadAllUserTicks" class = "button"> Download CSV of All Users Ticks </button>')
-
-
-$("#downloadAllUserTicks").click(function(){
-
-	$(this).text("loading");
-
-	console.log("Ive been clicked!");
-	
-	getPredictionForCurrentClimb();
-
-})
-
-
-// -------- SECTION FOR IS CLIMB ON A CLASSIC LIST --------- //
-
 //Determines what kind of page we're on
 var thisUrlIsTheCurrentPage = whatPageAmIOn()
 
@@ -96,7 +39,6 @@ else if (thisPageDetails.pageType() === "user"){
 	followFeatureOrchestration ()
 
 }
-
 
 
 else {
